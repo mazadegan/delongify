43072bc694c5020d5625652e02316fef65a5993f
mazadegan <azadeganmalek@gmail.com>
2022-08-09 16:31:35 -0400

    added get, api group, and static files

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
 
 func HelloHandler(c *gin.Context) {
 	c.String(200, "Hello, world!")
 }
 
+func PingHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
 func main() {
 	r := gin.Default()
 
-	r.GET("/", HelloHandler)
+	r.GET("/hello", HelloHandler)
 
-	r.Run(":3000")
+	api := r.Group("/api")
+	api.GET("/ping", PingHandler)
+
+	r.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	r.Run()
 }
