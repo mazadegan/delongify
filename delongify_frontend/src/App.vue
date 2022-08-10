68fbb0f7bca4ccf97b655e459ee88cba92e5ffbf
mazadegan <azadeganmalek@gmail.com>
2022-08-10 11:13:05 -0400

    added heroicons

--- delongify_frontend/src/App.vue
+++ delongify_frontend/src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
+  import { BeakerIcon } from '@heroicons/vue/outline'
   document.title = "Delongify | Home"
 </script>
 
 <template>
   <div>
-    <span class="text-xl">You are currently <span class="font-bold">TailwindCSS.</span></span>
+    <span class="flex items-center text-xl space-x-2">
+      <BeakerIcon class="w-8 h-8 pb-0.5 text-indigo-700"></BeakerIcon>
+      <span>
+        You are currently <span class="font-bold">TailwindCSS.</span>
+      </span>     
+    </span>
   </div>
 </template>
 
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+cd delongify_frontend
+npm run build
+cd ..
+git push heroku master
